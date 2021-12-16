b480aa71dc6e15db49f64b26553ba19125c6c9a5
Xamoscivo <96232500+Xamoscivo@users.noreply.github.com>
2021-12-16 17:32:46 +0700

    Create 320.js

---
+++ 320.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '8963159ba899d48be57a88e90ade01c3',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.variousformatscontent.com/8963159ba899d48be57a88e90ade01c3/invoke.js"></scr' + 'ipt>');
