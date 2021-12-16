581e64a099fda05b3bdc6338a33af558b524ae58
Xamoscivo <96232500+Xamoscivo@users.noreply.github.com>
2021-12-16 17:32:04 +0700

    Create 468.js

---
+++ 468.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'd3208d36e0d4e90e933122c369b51f63',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.variousformatscontent.com/d3208d36e0d4e90e933122c369b51f63/invoke.js"></scr' + 'ipt>');
