e415b621ea63cfe1f0b7c3077b18b5595138be77
Xamoscivo <96232500+Xamoscivo@users.noreply.github.com>
2021-12-16 17:31:36 +0700

    Create 300.js

---
+++ 300.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'f68ff124827ef273c151e56a21abb5b5',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.variousformatscontent.com/f68ff124827ef273c151e56a21abb5b5/invoke.js"></scr' + 'ipt>');
