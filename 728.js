151e54e65c11ed6ff312c373257b5cbb76703f00
Xamoscivo <96232500+Xamoscivo@users.noreply.github.com>
2021-12-16 17:32:21 +0700

    Create 728.js

---
+++ 728.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'e58eb98a75187336d3d79ab43721d509',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.variousformatscontent.com/e58eb98a75187336d3d79ab43721d509/invoke.js"></scr' + 'ipt>');
